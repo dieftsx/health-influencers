27e41d147f678f41c674e8ea8098bf8fed1996f0
dieftsx <diefersonfrontenddeveloper@gmail.com>
2025-01-14 13:49:32 -0300

    chore: useToast

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 
+
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,6 +12,10 @@ const nextConfig = {
     CONSENSUS_API_KEY: process.env.CONSENSUS_API_KEY,
     PERPLEXITY_API_KEY: process.env.PERPLEXITY_API_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
+    return config
   },
 }
 
